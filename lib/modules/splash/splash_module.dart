c0387168f663d21226d4cd44ba09067d2281f2ca
Enzo Zamineli <enzozamineli@gmail.com>
2025-09-22 14:12:39 -0300

    fix: Estava extension mas é pra ser implements

--- lib/modules/splash/splash_module.dart
+++ lib/modules/splash/splash_module.dart
@@ -2,7 +2,7 @@ import 'package:app_filmes/application/modules/module.dart';
 import 'package:app_filmes/modules/splash/splash_page.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 
-class SplashModule extends Module {
+class SplashModule implements Module {
   @override
   List<GetPage> routers = [
     GetPage(name: '/', page: () => SplashPage()),
